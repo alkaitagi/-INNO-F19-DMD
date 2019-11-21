b54adf838e51cc3dc4de3a4a52616dc459d69c6f
alkaitagi <alkaitagi@outlook.com>
2019-11-21 18:36:56 +0300

    Initial query selection implementation

--- launch.py
+++ launch.py
@@ -11,15 +11,7 @@ cur = conn.cursor()
 #       COURSE        CHAR(50),
 #       DEPARTMENT        CHAR(50));''')
 
-cur.execute(
-    "INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3421, 'Joel', 17, 'Computer Science', 'ICT')"
-)
-cur.execute(
-    "INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3422, 'Antony', 19, 'Electrical Engineering', 'Engineering')"
-)
-cur.execute(
-    "INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3423, 'Alice', 18, 'Information Technology', 'ICT')"
-)
+cur.execute("select * from student")
 
 conn.commit()
 conn.close()
---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import psycopg2
+import json
+
+
+def displayQueries(queries):
+    i = 0
+    print('Available queries:')
+    for query in queries:
+        i += 1
+        print(f'{i}. {query["display"]}')
+
+
+def loadQueries():
+    with open('queries/index.json') as file:
+        return json.load(file)
+
+
+con = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+cur = con.cursor()
+
+queries = loadQueries()
+displayQueries(queries)
+
+while True:
+    i = int(input('\nSelect: ')) - 1
+
+    with open(f'queries/{queries[i]["file"]}') as query:
+        cur.execute(query.read())
+
+    print('Executed')
+
+con.commit()
+con.close()
