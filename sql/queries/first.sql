ce64464966883fe451aa97f125d4b6a6d3ef2bb5
Andrey <F_A_M@list.ru>
2019-11-28 12:59:54 +0300

    upd first.sql

--- sql/queries/first.sql
+++ sql/queries/first.sql
@@ -1,7 +1,8 @@
 SELECT
-    DISTINCT employee.name,
+    employee.name,
     employee.surname,
-    employee.ssn
+    employee.ssn,
+	MAX(date) as datee
 from
     attends,
     employee,
@@ -20,3 +21,24 @@ where
         and patient.gender = 'female' and
         employee.type = 'doctor'
     )
+GROUP BY employee.ssn
+HAVING to_char(MAX(date), 'yyyy-mm-dd')=to_char((SELECT
+    MAX(date)
+from
+    attends,
+    employee,
+    patient
+where
+    attends.employee_ssn = employee.ssn
+    and attends.patient_ssn = %(arg0)s
+    and (
+        (
+            employee.name LIKE 'M%%'
+            or employee.name LIKE 'L%%'
+        ) <> (
+            employee.surname LIKE 'M%%'
+            or employee.surname LIKE 'L%%'
+        ) and patient.ssn = attends.patient_ssn
+        and patient.gender = 'female' and
+        employee.type = 'doctor'
+    )), 'yyyy-mm-dd')
