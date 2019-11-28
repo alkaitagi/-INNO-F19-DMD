1509da6c6b233b1e0cd6744bf42d404da9413866
Ivan <i.abramov@innopolis.university>
2019-11-28 13:39:39 +0300

    edit 2 query

--- sql/queries/second.sql
+++ sql/queries/second.sql
@@ -1,5 +1,5 @@
-select  employee_ssn, extract(dow from date) as Day, (extract(hour from date)) as Hour, COUNT(*) as Total, TRUNC(COUNT(*)/52.0,5) as Average
+select  employee_ssn, extract(dow from date) as Day, (extract(hour from date)) as Hour, COUNT(*) as Total, TRUNC(COUNT(*)/52.0, 5) as Average
 from attends
-where (extract(year from date) BETWEEN 2015 and 2016)
+where (extract(year from date) BETWEEN %(arg0)s -  1 and %(arg0)s)
 GROUP BY employee_ssn,  Day, Hour
 ORDER BY employee_ssn, Day, Hour
